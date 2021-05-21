3109fa3b29068d2a6b4f0f30e8962f6c7756de95
fox091 <joshmassey25@gmail.com>
2021-05-20 23:45:44 -0400

    EX-6 Refactored weapons to be a gameobject with weapon script attached

--- Assets/Items/Equipment/Equipment.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Items.Equipment
-{
-    public abstract class Equipment : Item
-    {
-        public GameObject EquippableGameObject;
-    }
-}
--- Assets/Items/Equipment/Weapons/Weapon.cs
+++
@@ -1,7 +0,0 @@
-namespace Items.Equipment.Weapons
-{
-    public abstract class Weapon : Equipment
-    {
-        public abstract int Damage { get; }
-    }
-}
