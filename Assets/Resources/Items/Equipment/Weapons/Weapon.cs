60ffa3ad33f7f504a22953b4fbac0d9b255c3074
fox091 <joshmassey25@gmail.com>
2021-05-20 23:48:34 -0400

    EX-6 remove unnecessary stuff from Weapon script

--- Assets/Resources/Items/Equipment/Weapons/Weapon.cs
+++ Assets/Resources/Items/Equipment/Weapons/Weapon.cs
@@ -3,16 +3,4 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public int Damage;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
