bfb00312e5781a127de1ec015efbee9e7a689a8d
fox091 <joshmassey25@gmail.com>
2021-06-15 21:32:33 -0400

    EX-25 Move some inventory stuff around and move inventory panel into a scrollrect

--- Assets/Resources/Inventory/InventoryUi.cs
+++ Assets/Resources/Inventory/InventoryUi.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
+    public GameObject TEMPHealthPotion;
     public GameObject ItemActionsPanel;
     public GameObject ItemActionButtonPrefab;
     private List<GameObject> _itemActionButtons = new List<GameObject>();
@@ -11,6 +12,17 @@ public class InventoryUI : MonoBehaviour
     void OnUpdate()
     {
         ItemActionsPanel.SetActive(false);
+    }
+
+    private void Start()
+    {
+        Instantiate(TEMPHealthPotion, new Vector3(0, 0), Quaternion.identity, transform);
+        Instantiate(TEMPHealthPotion, new Vector3(0, 100), Quaternion.identity, transform);
+        Instantiate(TEMPHealthPotion, new Vector3(0, 200), Quaternion.identity, transform);
+        Instantiate(TEMPHealthPotion, new Vector3(0, 300), Quaternion.identity, transform);
+        Instantiate(TEMPHealthPotion, new Vector3(0, 400), Quaternion.identity, transform);
+        Instantiate(TEMPHealthPotion, new Vector3(0, 600), Quaternion.identity, transform);
+        Instantiate(TEMPHealthPotion, new Vector3(0, 700), Quaternion.identity, transform);
     }
 
     // TODO: Register listeners
