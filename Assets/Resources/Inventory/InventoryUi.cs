2217079c8394983c2c63cfa857245dd8546dfa1d
fox091 <joshmassey25@gmail.com>
2021-06-17 00:43:28 -0400

    EX-25 Add real health potion image, fix inventory scrollrect with grid layout, get dynamic spawns working

--- Assets/Resources/Inventory/InventoryUi.cs
+++ Assets/Resources/Inventory/InventoryUi.cs
@@ -1,28 +1,30 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
     public GameObject TEMPHealthPotion;
-    public GameObject ItemActionsPanel;
-    public GameObject ItemActionButtonPrefab;
     private List<GameObject> _itemActionButtons = new List<GameObject>();
     private List<GameObject> _items = new List<GameObject>();
-    
-    void OnUpdate()
-    {
-        ItemActionsPanel.SetActive(false);
-    }
 
     private void Start()
     {
-        Instantiate(TEMPHealthPotion, new Vector3(0, 0), Quaternion.identity, transform);
-        Instantiate(TEMPHealthPotion, new Vector3(0, 100), Quaternion.identity, transform);
-        Instantiate(TEMPHealthPotion, new Vector3(0, 200), Quaternion.identity, transform);
-        Instantiate(TEMPHealthPotion, new Vector3(0, 300), Quaternion.identity, transform);
-        Instantiate(TEMPHealthPotion, new Vector3(0, 400), Quaternion.identity, transform);
-        Instantiate(TEMPHealthPotion, new Vector3(0, 600), Quaternion.identity, transform);
-        Instantiate(TEMPHealthPotion, new Vector3(0, 700), Quaternion.identity, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
+        Instantiate(TEMPHealthPotion, transform);
     }
 
     // TODO: Register listeners
